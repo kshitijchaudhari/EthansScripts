b29180043fbc0ebbfd393ae14fed83dec8484fc6
Kshitij <kshitij@localhost.localdomain>
2020-02-16 08:55:29 -0800

    Recreating Ethans scripts in Bash

---
+++ calc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"
+echo "The file name of the this script is $0"
+A=$(( 2 + 3 )) 
+B=$(( 4 - 5 ))
+echo "$A"
+echo "The value of B is $B"
+
